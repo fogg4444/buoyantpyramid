810b000d46e1985a1abffc7b33a77b588954c5da
Erick Paepke <epaepke@gmail.com>
2016-03-10 16:43:38 -0800

    (feat) Initiate express server

---
+++ distribution/server/server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var config = require('./config/config')
+
+var app = express();
+
+// connect to mongo database named "dropnet"
+mongoose.connect(config.mongoURL);
+
+// configure our server with all the middleware and routing
+require('./config/middleware.js')(app, express);
+require('./config/routes.js')(app, express);
+
+// start listening to requests on port 8000
+app.listen(config.port, function() {console.log('listening on port: ', config.port)});
+
+// export our app for testing and flexibility, required by index.js
+module.exports = app;
