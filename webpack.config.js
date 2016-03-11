101608e9907a8f54f458793c88cbbe102ae4e846
Silverhawk <sondra.silverhawk@hush.com>
2016-03-11 15:39:46 -0800

    (feat) Add files and dependencies for webpack

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ClosureCompilerPlugin = require('webpack-closure-compiler');
+
+module.exports = {
+  devtool: 'eval-source-map',
+  context: path.join(__dirname, 'app'),
+  entry: './main.js',
+  output: {
+    path: path.join(__dirname, '/dist/'),
+    filename: 'dist',
+    publicPath: '/'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'app/index.tpl.html',
+      inject: 'body',
+      filename: 'index.html'
+    }),
+    new ClosureCompilerPlugin({
+      compiler: {
+        language_in: 'ECMASCRIPT6',
+        language_out: 'ECMASCRIPT5',
+        compilation_level: 'ADVANCED'
+      },
+      concurrency: 3,
+    }),
+    new webpack.optimize.OccurenceOrderPlugin(),
+  ],
+  module: {
+    loaders: [{
+      test: /\.js?$/,
+      exclude: /node_modules/,
+      loader: 'babel'
+    }, {
+      test: /\.json?$/,
+      loader: 'json'
+    }, {
+      test: /\.css$/,
+      loader: 'style!css?modules&localIdentName=[name]---[local]---[hash:base64:5]'
+    }]
+  }
+};
