fc052e8421d588edcf0ef5247f8f858292cabbde
Erick Paepke <epaepke@gmail.com>
2016-03-11 19:36:49 -0800

    (cleanup) Correct module exports formatting

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -1,9 +1,11 @@
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 
-module.exports = function (app, express) {
+var middleware = function (app, express) {
   app.use(morgan('dev'));
   app.use(bodyParser.urlencoded({extended: true}));
   app.use(bodyParser.json());
   app.use(express.static(__dirname + '/../../client'));
 };
+
+module.exports = middleware;
