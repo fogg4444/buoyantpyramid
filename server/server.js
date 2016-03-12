50609e9a361e55e2d1a3a90ea554b28008a6f386
Nick Echols <stere.ereo@gmail.com>
2016-03-11 18:32:56 -0800

    (cleanup) clean up code to pass linter

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var mongoose = require('mongoose');
-var config = require('./config/config')
+var config = require('./config/config');
 
 var app = express();
 
@@ -17,7 +17,7 @@ var normalizePort = function(val) {
     return port;
   }
   return false;
-}
+};
 
 // Get port from environment and store in Express.
 config.port = normalizePort(process.env.PORT || config.port);
@@ -27,7 +27,7 @@ require('./config/middleware.js')(app, express);
 require('./config/routes.js')(app, express);
 
 // start listening to requests on port 8000
-app.listen(config.port, function() {console.log('listening on port: ', config.port)});
+app.listen(config.port, function() { console.log('listening on port: ', config.port); });
 
 // export our app for testing and flexibility, required by index.js
 module.exports = app;
