4ab6ea10b9e7df2ed384a1d09f58889e28d09c97
Erick Paepke <epaepke@gmail.com>
2016-03-14 16:06:20 -0700

    (feat) Add fetch songs from group function

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,5 +1,7 @@
 // var groupController = require('../groups/groupController.js');
 var helpers = require('./helpers.js');
+var Song = require('../models/songs')
+var Group = require('../models/groups')
 // var playlistController = require('../playlists/playlistController.js');
 // var songController = require('../songs/songController.js');
 // var userController = require('../users/userController.js');
@@ -9,6 +11,9 @@ var routing = function (app, express) {
   // app.post('/api/users/signup', userController.signup);
   // app.get('/api/users/signedin', userController.checkAuth);
 
+  app.post('/api/songs/', Song.addSong);
+  app.post('/api/groups/', Group.createGroup);
+  app.get('/api/songs/:id', Group.fetchSongs);
   // Handle error logging of requests that are destined for above routes
   app.use(helpers.errorLogger);
   app.use(helpers.errorHandler);
