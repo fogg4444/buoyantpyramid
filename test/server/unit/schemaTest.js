434a65d966d6be5a1ea1406773174565c7cb70a4
Nick Echols <stere.ereo@gmail.com>
2016-03-15 11:02:23 -0700

    (test) Write tests for song schema and controller

--- test/server/unit/schemaTest.js
+++ test/server/unit/schemaTest.js
@@ -3,9 +3,29 @@ var Sequelize = require('sequelize');
 var dbModels = require('../../../server/db/database.js');
 var Song = dbModels.Song; 
 var User = dbModels.User; 
+var Group = dbModels.Group; 
 dbModels.db.options.logging = false;
 
 
+// rebuild test database
+before(function (done) {
+  User.sync({force: true})
+  .then(function() {
+    return Group.sync({force: true});
+  })
+  // .then(function() {
+  //   return Playlist.sync({force: true});
+  // })
+  .then(function() {
+    return Song.sync({force: true});
+  })
+  // .then(function() {
+  //   return UserGroups.sync({force: true});
+  // });
+  .then(function() {
+    done();
+  });
+});
 
 describe('User Model', function () {
   it('User should be a Sequelize model', function () {
@@ -20,6 +40,18 @@ describe('User Model', function () {
   });
 });
 
+describe('Group Model', function () {
+  it('should be a Sequelize model', function () {
+    expect(Group).to.be.instanceOf(Sequelize.Model);
+  });
+  
+  it('should have a schema with fields: name, bannerUrl', function (done) {
+    Group.describe().then(function(schema) {
+      expect(schema).to.include.keys('name', 'bannerUrl');
+      done();
+    });
+  });
+});
 
 describe('Song Model', function () {
   it('should be a Sequelize model', function () {
