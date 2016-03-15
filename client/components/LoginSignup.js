2d27549bebfbc01057afe7923ed642a579bec65a
Silverhawk <sondra.silverhawk@hush.com>
2016-03-14 20:16:13 -0700

    (cleanup) Remove ghost code, rename button

--- client/components/LoginSignup.js
+++ client/components/LoginSignup.js
@@ -1,28 +1,3 @@
-// import React, { PropTypes } from 'react';
-// import { browserHistory, Router, Route, Link } from 'react-router';
-
-// class Login extends React.Component {
-//   render() {
-//     return (
-//       <div>
-//         <h3>Sign in or create an account</h3>
-//         <input ref={ node => { this.input = node; } } />
-//         <button onClick={ () => {
-//           store.dispatch({
-//             type: 'UPDATE_USER',
-//             email: this.input.value,
-//           });
-//         } }>
-//         Login
-//         </button>
-//       </div>
-//     );
-//   }
-// }
-
-// export default Login;
-
-
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { updateUser } from '../actions';
@@ -44,7 +19,7 @@ let LoginSignup = ({ dispatch }) => {
           input = node;
         }} />
         <button type="submit">
-          Add Todo
+          Login
         </button>
       </form>
     </div>
