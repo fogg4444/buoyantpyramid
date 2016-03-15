d92dcb29f626dbcda87de815cb4728046a05f723
Erick Paepke <epaepke@gmail.com>
2016-03-15 14:33:11 -0700

    (feat) Add user signup function

--- server/models/user.js
+++ server/models/user.js
@@ -1,24 +1,36 @@
 var db = require('../db/database');
 var User = db.User;
+var jwt = require('jwt-simple');
 
 // TODO: Add authentication
-var createUser = function(req, res, next) {
-  var email = req.body.email;
+var signup = function (req, res, next) {
   var displayName = req.body.displayName;
+  var email = req.body.email;
   var password = req.body.password;
 
-  User.create({
-    email: email,
-    displayName: displayName,
-    password: password
-  }).then(function(user) {
-    res.json(user);
-  })
-  .catch(function(err) {
-    next(err); 
-  });
-};
+  User.findOne({where: {email: email}})
+    .then(function (user) {
+      if (user) {
+        throw new Error('User already exists!');
+      } else {
+        return User.create({
+          displayName: displayName,
+          email: email,
+          password: password
+        });
+      }
+    })
+    .then(function (user) {
+      var token = jwt.encode(user, 'secret');
+      res.json({token: token});
+    })
+    .catch(function (error) {
+      next(error);
+    });
+}
+
+var signin = function()
 
 module.exports = {
-  createUser: createUser
+  signup: signup
 };
