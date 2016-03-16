7e4af9b121a6333c81e868ec67b19fd34da6edf4
Silverhawk <sondra.silverhawk@hush.com>
2016-03-16 16:22:57 -0700

    (cleanup) Finish cleanup and get min working!
    closes #26

--- client/app/songs/songs.js
+++ client/app/songs/songs.js
@@ -1,14 +1,18 @@
 angular.module('jam.songs', [])
 
-.controller('SongsController', ['$scope', 'Songs', 'Auth', function ($scope, Songs, Auth) {
+.controller('SongsController', ['$scope', '$location', 'Songs', 'Auth', function ($scope, loc, Songs, Auth) {
   // When user adds a new link, put it in the collection
   $scope.data = {};
-  $scope.data.user = Auth.getUserData();
-  Songs.getAllSongs($scope.data.user.currentGroupId)
-  .then(function (res) {
-    $scope.data.songs = res;
-  })
-  .catch(function (error) {
-    console.error(error);
-  });
+  $scope.user = Auth.getUserData();
+  if ($scope.user) {
+    Songs.getAllSongs($scope.user.currentGroupId)
+    .then(function (res) {
+      $scope.data.songs = res;
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+  } else {
+    loc.path('/login');
+  }
 }]);
