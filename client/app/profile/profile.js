29ce5715f0b9b8a177d070aece44148f369c1c1e
Silverhawk <sondra.silverhawk@hush.com>
2016-03-18 10:26:38 -0700

    (feat) Upload avatar image using ng-file-upload

--- client/app/profile/profile.js
+++ client/app/profile/profile.js
@@ -1,18 +1,37 @@
 angular.module('jam.profile', [])
 
-.controller('ProfileController', ['$scope', '$location', 'Auth', 'FileUploader',
-function ($scope, loc, Auth, Up) {
+.controller('ProfileController', ['$scope', '$location', '$window', '$timeout', 'Auth', 'Upload',
+function ($scope, loc, win, to, Auth, Up) {
   Auth.getUserData()
   .then(function (userData) {
     $scope.user = userData;
   })
   .catch(console.error);
-  
-  $scope.uploader = new Up();
-  $scope.uploading = false;
+
+  $scope.uploadFiles = function(file, errFiles) {
+    $scope.f = file;
+    $scope.errFile = errFiles && errFiles[0];
+    if (file) {
+      file.upload = Up.upload({
+        url: '/api/upload',
+        data: {file: file}
+      });
+
+      file.upload.then(function (response) {
+        to(function () {
+          file.result = response.data;
+        });
+      }, function (response) {
+        if (response.status > 0) {
+          $scope.errorMsg = response.status + ': ' + response.data;
+        }
+      }, function (evt) {
+        file.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
+      });
+    }
+  };   
 
   $scope.updateProfile = function () {
-    $scope.uploading = true;
     Auth.updateProfile($scope.user)
     .then(function (res) {
       console.log('Profile updated', res.data.user);
