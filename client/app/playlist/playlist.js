4a91b90eab334b34ec16efcb589d9e2be97b560a
Silverhawk <sondra.silverhawk@hush.com>
2016-03-19 15:50:09 -0700

    (feat) Add modal to create new playlist

--- client/app/playlist/playlist.js
+++ client/app/playlist/playlist.js
@@ -1,5 +1,16 @@
 angular.module('jam.playlist', [])
-.controller('PlaylistController', ['$scope', function ($scope) {
+.controller('PlaylistController', ['$scope', 'Playlists', function ($scope, PL) {
   $scope.currentPlaylist = {};
   
+  $scope.toggleModal = function () {
+    $scope.modalShown = !$scope.modalShown;
+  };
+
+  $scope.createPlaylist = function () {
+    PL.createPlaylist($scope.newPlaylist)
+    .then(function (playlist) {
+      console.log("New! ", playlist);
+    });
+  };
+
 }]);
