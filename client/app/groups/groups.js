abaee828be6b55c967076fa65e7c4c290f23ca39
Erick Paepke <epaepke@gmail.com>
2016-03-18 19:24:11 -0700

    (fix) Remove comments from modal template

--- client/app/groups/groups.js
+++ client/app/groups/groups.js
@@ -2,13 +2,23 @@ angular.module('jam.groups', [])
 
 .controller('GroupsController', ['$scope', 'Auth', 'Groups', function($scope, Auth, Groups) {
   $scope.user = {};
+  $scope.newGroup = {};
   $scope.data = {};
+  $scope.modalShown = false;
 
   $scope.filterGroups = function(group) {
     return function(group) {
       return group.id !== $scope.user.currentGroupId;
     }
-  }
+  };
+
+  $scope.toggleModal = function() {
+    $scope.modalShown = !$scope.modalShown;
+  };
+
+  $scope.createGroup = function() {
+    console.log("new Group", $scope.newGroup);
+  };
 
   Auth.getUserData()
   .then(function (userData) {
