c3f54449e1287d228fb4764ba18fb1fc6749445c
Nick Echols <stere.ereo@gmail.com>
2016-03-18 18:21:31 -0700

    (cleanup) get rid of ghost code

--- client/app/upload/upload.js
+++ client/app/upload/upload.js
@@ -7,13 +7,6 @@ angular
     var totalToUpload = 0;
     var totalUploaded = 0;
     var totalPercent = 0;
-    // upload later on form submit or something similar
-    $scope.submit = function() {
-      // console.log('Submit', $scope.form.file);
-      // if ($scope.form.file.$valid && $scope.file) {
-      //   $scope.upload($scope.file);
-      // }
-    };
 
     var findTotalPercent = function() {
       var total = 0;
@@ -24,7 +17,6 @@ angular
       }
 
       totalPercent = Math.ceil(total / (totalToUpload));
-      console.log('totalpercent ' + totalPercent);
       $scope.progressbar.set(totalPercent);
       if (totalPercent === 100) {
         $scope.progressbar.complete();
@@ -34,19 +26,14 @@ angular
     var throttledTotal = _.throttle(findTotalPercent, 250);
 
     $scope.addToQueue = function(files) {
-      // console.log(files);
       for (file in files) {
         $scope.queue.push(files[file]);
-        console.dir(file);
       }
-      console.log($scope.queue);
     };
 
     $scope.queue = [];
-    $scope.uploadingCount = 0;
-
+    
     $scope.removeFile = function(index) {
-      console.log('Remove file: ', file);
       if (index > -1) {
         $scope.queue.splice(index, 1);
       }
@@ -54,8 +41,6 @@ angular
 
     // upload on file select or drop
     $scope.upload = function(file) {
-      // console.log('Upload', file);
-      
       var groupId;
       Auth.getUserData()
       .then(function(user) {
@@ -64,20 +49,12 @@ angular
           data: { file: file }
         }).then(function(resp) {
           totalUploaded++;
-          // var totalPercent = Math.ceil(100 * (totalUploaded / totalToUpload));
-          // console.log('Success ' + resp.config.data.file.name + 'uploaded. Response: ' + resp.data);
-          // $scope.progressbar.set(totalPercent);
-          // console.log('totalpercent ' + totalPercent);
         }, function(resp) {
-          console.log('Error status: ' + resp.status);
+          console.error('Error status: ' + resp.status);
           totalUploaded++;
-          // $scope.progressbar.set(Math.ceil(100 * (totalUploaded / totalToUpload)));
         }, function(evt) {
           var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
-          // console.log('progress: ' + progressPercentage + '% ' + evt.config.data.file.name);
           file['progressPercentage'] = progressPercentage;
-          // findTotalPercent();
-          // console.log($scope.queue);
           throttledTotal();
         });
       });
@@ -85,15 +62,12 @@ angular
     // for multiple files:
     $scope.uploadFiles = function() {
       $scope.progressbar.set(0);
-      // console.log('Upload files', $scope.queue);
       if ($scope.queue && $scope.queue.length) {
         totalToUpload = $scope.queue.length;
         totalUploaded = 0;
         for (var i = 0; i < $scope.queue.length; i++) {
-          // console.log('file: ', $scope.queue[i]);
           $scope.upload($scope.queue[i]);
         }
       }
     };
-     // $scope.progressbar.start();
   }]);
