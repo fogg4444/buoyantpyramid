102d853a564d287d8c59fdb4f7115ab6dff4ff46
Erick Paepke <epaepke@gmail.com>
2016-03-19 09:45:18 -0700

    (feat) Set current group after new group creation

--- client/app/groups/groups.js
+++ client/app/groups/groups.js
@@ -1,28 +1,45 @@
 angular.module('jam.groups', [])
 
-.controller('GroupsController', ['$scope', 'Auth', 'Groups', function($scope, Auth, Groups) {
+.controller('GroupsController', ['$scope', '$route', 'Auth', 'Groups', function($scope, $route, Auth, Groups) {
   $scope.user = {};
   $scope.newGroup = {};
   $scope.data = {};
   $scope.modalShown = false;
 
-  $scope.filterGroups = function(group) {
+  $scope.filterGroups = function (group) {
     return function(group) {
       return group.id !== $scope.user.currentGroupId;
     }
   };
 
-  $scope.toggleModal = function() {
+  $scope.toggleModal = function () {
     $scope.modalShown = !$scope.modalShown;
   };
 
-  $scope.createGroup = function() {
+  $scope.createGroup = function () {
     Groups.createGroup($scope.newGroup)
     .then(function (group) {
-      Groups.addUser(group.id, $scope.user.id);
+      Groups.addUser(group.id, $scope.user.id)
+      .then(function () {
+        $scope.modalShown = false;
+        $scope.user.currentGroupId = group.id;
+        $scope.user.currentGroup = group;
+        $scope.updateProfile($scope.user)
+      });
     });
   };
 
+  $scope.updateProfile = function () {
+    return Auth.updateProfile($scope.user)
+    .then(function (res) {
+      $scope.user = res.data.user;
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+  };
+
+  // Load groups and group users
   Auth.getUserData()
   .then(function (userData) {
     $scope.user = userData;
