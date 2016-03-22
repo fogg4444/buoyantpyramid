74460e21423f6827eefe728411cfa7881da6a8c3
Erick Paepke <erickpaepke@Erick-Paepkes-MacBook-Pro.local>
2016-03-21 19:42:27 -0700

    (feat) Implement add song function

--- server/controllers/tag.js
+++ server/controllers/tag.js
@@ -3,7 +3,32 @@ var Song = db.Song;
 var User = db.User;
 var Tag = db.Tag;
 
-var addSong = function(req, res, next) {
+var addSong = function (req, res, next) {
+	var time = req.body.time;
+  var note = req.body.note;
+  var userId = req.body.userId;
+  var songId = req.params.id;
+
+  Tag.create({
+    time: time,
+    note: note,
+    userId: userId,
+    songId: songId
+  }, {
+    include: {
+      model: Song,
+      model: User
+    }
+  })
+  .then(function(song) {
+    res.json(song);
+  })
+  .catch(function(err) {
+    next(err);
+  });
+};
+
+var deleteSong = function (req, res, next) {
 };
 
 module.exports = {
