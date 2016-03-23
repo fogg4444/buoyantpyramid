c63cf1b793f3bc3cafb882902e0b9b004fab0f13
Silverhawk <sondra.silverhawk@hush.com>
2016-03-23 15:17:10 -0700

    (fix) Player starts out empty

--- client/app/player/player.js
+++ client/app/player/player.js
@@ -1,30 +1,28 @@
 angular.module('jam.player', [])
 .controller('PlayerController', ['$scope', 'ngAudio', 'Songs', function($scope, audio, Songs) {
   $scope.playlist = Songs.getSoundsAndIndex();
-  $scope.placeholderSound = audio.load('http://mattyluv.com/mp3/hickey_various/Hickey-ElFarolito.mp3');
-  $scope.sound = $scope.playlist.sounds ? $scope.playlist.sounds[$scope.playlist.index] : $scope.placeholderSound;
+  $scope.sound = $scope.playlist.sounds ? $scope.playlist.sounds[$scope.playlist.index] : null;
   
-  $scope.togglePlay = function () {
-    if ($scope.sound.paused) {
-      $scope.sound.play();
-    } else {
-      $scope.sound.pause();
-    }
-  };
+  // $scope.togglePlay = function () {
+  //   if ($scope.sound.paused) {
+  //     $scope.sound.play();
+  //   } else {
+  //     $scope.sound.pause();
+  //   }
+  // };
 
   var changeSong = function() {
-    $scope.sound.stop();
+    if ($scope.sound) {
+      $scope.sound.stop();
+    }
     $scope.playlist = Songs.getSoundsAndIndex();
     $scope.sound = $scope.playlist.sounds[$scope.playlist.index];
-    $scope.sound.play();
     $scope.sound.complete(function() {
       Songs.nextIndex();
     });
+    $scope.sound.play();
   };
 
-  $scope.sound.complete(function() {
-    Songs.nextIndex();
-  });
 
   Songs.registerObserverCallback(changeSong);
 }]);
