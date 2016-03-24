4e3f32fb912d6be2289e7600ebea53add0cb3ab5
Brian Fogg <fogg4444@gmail.com>
2016-03-24 11:19:57 -0700

    (feat) Add install and configuration instructions to README

--- README.md
+++ README.md
@@ -9,7 +9,6 @@
 Audio organization system for stoned musicians
 
 ## Team
-
   - __Product Owner__: Brian Fogg
   - __Scrum Master__: Nick Echols
   - __Development Team Members__: Sondra Silverhawk, Erick Paepke
@@ -23,9 +22,6 @@ Audio organization system for stoned musicians
     1. [Tasks](#tasks)
 1. [Team](#team)
 1. [Contributing](#contributing)
-
-## Usage
-
 
 
 ## Requirements
@@ -50,9 +46,45 @@ sh ./pomander.sh
 ```
 
 
+
+## Usage
 ### Server configuration
 
-Open distribution/server/config/config.example.js and use template to create config.js.
+
+1. Update config files on primary server
+  (do this manually)
+2. Update config files on compression_server
+  (do this manually)
+
+from project root
+
+3. Install dependencies
+
+  npm install;
+  bower install;
+  cd ./compression_server;
+  npm install;
+  cd ..;
+
+
+5. Start postgres database
+  
+  Start:
+  pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start
+  
+  Stop:
+  pg_ctl -D /usr/local/var/postgres stop -s -m fast
+
+6. Clear all databases
+  
+  drop table users cascade; drop table groups cascade; drop table users; drop table groups; drop table "userGroups"; drop table playlists; drop table songs cascade; drop table "playlistSongs";
+
+7. Start primary server
+  gulp
+
+8. Start compression server
+  cd ./compression_server;
+  npm start
 
 
 ### Roadmap
---
+++ test/compression_server/compressionTest.js
@@ -0,0 +1,59 @@
+var chai = require('chai');
+var assert = chai.assert;
+var fs = require('fs');
+var request = require('request');
+var path = require('path');
+
+console.log('--- 1 --- Run Compression Server Tests!');
+
+
+describe('hooks', function() {
+  before(function() {
+    // runs before all tests in this block
+    console.log('--- 1.1 --- Before: download test.wav');
+
+    var testWavUrl = 'https://s3-us-west-1.amazonaws.com/jamrecordtest/indextest/testSourceFiles/test1.wav';
+    var destinationPath = path.join(__dirname + '/testFiles/test1.wav');
+    console.log(destinationPath);
+
+    request.get(testWavUrl)
+      .on('data', function(chunk) {
+        console.log(chunk);
+      })
+      .pipe( fs.createWriteStream(destinationPath) );
+    console.log('--- 1.2 --- Download completed');
+    
+  });
+
+  after(function() {
+    // runs after all tests in this block
+  });
+
+  beforeEach(function() {
+    // runs before each test in this block
+  });
+
+  afterEach(function() {
+    // runs after each test in this block
+  });
+
+  it('it should work', function () {
+    assert.equal(1, 1);
+  })
+
+  // test cases
+});
+
+
+// describe('Download test.wav', function() {
+
+
+
+//   // describe('#indexOf()', function () {
+//   //   it('should return -1 when the value is not present', function () {
+//   //     assert.equal(1, 1);
+//   //   });
+//   // });
+// });
+
+// https://s3-us-west-1.amazonaws.com/jamrecordtest/indextest/testSourceFiles/test1.wav
