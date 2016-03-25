93f1639eca6057305a28ce51128d341f142ba65d
Nick Echols <stere.ereo@gmail.com>
2016-03-25 12:57:54 -0700

    (cleanup) get rid of unnecessary logs and ghost code

--- server/controllers/songController.js
+++ server/controllers/songController.js
@@ -12,10 +12,6 @@ AWS.config.update({
 });
 var s3 = new AWS.S3();
 var bucketAddress = s3.endpoint.protocol + '//' + awsConfig.bucket + '.' + s3.endpoint.hostname + s3.endpoint.pathname;
-console.log('s3 bucket address is : ' + bucketAddress);
-
-var s3deleteAsync = Promise.promisify(s3.deleteObjects);
-
 
 var addCompressedLink = function(req, res, next) {
   console.log('--- Add Compressed Link ---', req.body);
@@ -49,18 +45,8 @@ var addSong = function(req, res, next) {
   });
 };
 
-// var getSongByFilename = function(req, res, next) {
-//   var filename = req.params.filename;
-//   var url = path.resolve(__dirname + '/../uploadInbox/' + filename);
-//   res.sendFile(url);
-// };
-
 var s3delete = function(song) {
   // delete both original and compressed files from aws
-  console.log('song address before replace: ' + song.address);
-  console.log('song address after replace: ' + song.address.replace(bucketAddress, ''));
-  
-  
   var params = {
     Bucket: awsConfig.bucket, /* required */
     Delete: { /* required */
@@ -87,7 +73,6 @@ var s3delete = function(song) {
 
 
 var deleteSong = function(req, res, next) {
-  // Only deletes from the database. FILES ARE STILL ON S3!
   var songId = req.params.id;
 
   SongModel.getSong(songId)
@@ -113,6 +98,5 @@ var deleteSong = function(req, res, next) {
 module.exports = {
   addCompressedLink: addCompressedLink,
   addSong: addSong,
-  // getSongByFilename: getSongByFilename,
   deleteSong: deleteSong
 };
