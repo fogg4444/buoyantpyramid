226c8a8d0091ec85f5298e637a82670b2693e3da
Erick Paepke <epaepke@gmail.com>
2016-03-26 12:29:22 -0700

    (feat) Update individual song page to show comment markers

--- client/app/song/song.js
+++ client/app/song/song.js
@@ -1,6 +1,6 @@
 angular.module('jam.song', [])
 
-.controller('SongController', ['$scope', '$location', 'Songs', 'Users', function ($scope, loc, Songs, Users) {
+.controller('SongController', ['$scope', '$location', '$route', 'Songs', 'Users', function ($scope, loc, $route, Songs, Users) {
   // When user adds a new link, put it in the collection
   $scope.song = Songs.getSongClicked();
   $scope.audio = Songs.getPlayer();
@@ -8,10 +8,27 @@ angular.module('jam.song', [])
   $scope.commentTime = null;
   $scope.pinningComment = false;
   $scope.user = {};
+  $scope.comments = [];
+  $scope.width = '1000px';
+  
+  var svgHeight = '200';
+  var svgHeight2 = '20';
+
+  var svgWidth = '1000';
+  var barPadding = '1';
+  var svg = createSvg('.visualizer', svgHeight, svgWidth);
+  var commentMarkers = createSvg('.comment-pin', svgHeight2, svgWidth);
 
   Users.getUserData()
   .then(function (user) {
     $scope.user = user;
+  })
+  .then(function () {
+    Songs.getComments($scope.song.id)
+    .then(function (comments) {
+      $scope.comments = comments;
+      addCommentMarker(comments);
+    });
   });
 
   // mock data
@@ -23,10 +40,92 @@ angular.module('jam.song', [])
       90, 100, 120, 120, 100, 120, 115, 120, 150, 100, 50, 20, 20, 30, 30, 60, 80, 140, 180,
        90, 100, 120, 120, 100, 120, 115, 120, 150, 100, 50, 20, 20, 30, 50, 1, 10, 30, 30, 60, 80, 140, 180];
 
-  var svgHeight = '200';
-  var svgWidth = '1000';
-  var barPadding = '1';
-  $scope.width = '1000px';
+  $scope.addComment = function (comment) {
+    var time = Math.floor($scope.commentTime * $scope.audio.duration);
+    Songs.addComment({note: comment, time: time, userId: $scope.user.id}, $scope.song.id)
+    .then(function (comment) {
+      $scope.comments.push(comment);
+      addCommentMarker($scope.comments);
+    })
+    $scope.pinningComment = false;
+    $scope.comment = '';
+    
+  };
+
+  var addCommentMarker = function(comments) {
+    commentMarkers.selectAll('rect')
+      .data(comments)
+      .enter()
+      .append('rect')
+      .attr('x', function (d, i) {
+        return d.time / $scope.duration * svgWidth;
+      })
+      .attr('width', svgWidth / frequencyData.length - barPadding);
+
+    commentMarkers.selectAll('rect')
+      .data($scope.comments)
+      .attr('x', function (d, i) {
+        return d.time / $scope.duration * svgWidth;
+      })
+      .attr('y', function(d) {
+        return svgHeight2 - 10;
+      })
+      .attr('height', function(d) {
+        return 100;
+      })
+      .transition()
+      .duration(600)
+      .attr('fill', function(d, i) {
+        return 'rgb(0, 0, ' + 220 + ')';
+      });
+  };
+
+  function createSvg(parent, height, width) {
+    return d3.select(parent).append('svg').attr('height', height).attr('width', width);
+  };
+
+  $scope.pinComment = function () {
+    $scope.commentTime = $scope.audio.currentTime / $scope.audio.duration;
+    $scope.pinningComment = true;
+  };
+
+  $scope.setPlayTime = function (e) {
+    div = document.getElementsByClassName('visualizer')[0];
+    var x = e.clientX - div.offsetLeft;
+    $scope.audio.currentTime = $scope.audio.duration * x / svgWidth;
+  };
+
+  svg.selectAll('rect')
+    .data(frequencyData)
+    .enter()
+    .append('rect')
+    .attr('x', function (d, i) {
+     return i * (svgWidth / frequencyData.length);
+    })
+    .attr('width', svgWidth / frequencyData.length - barPadding);
+
+  // Continuously loop and update chart with frequency data.
+  function renderFlow() {
+     // Update d3 chart with new data.
+    svg.selectAll('rect')
+      .data(frequencyData)
+      .attr('y', function(d) {
+         return svgHeight - d;
+      })
+      .attr('height', function(d) {
+         return d;
+      })
+      .transition()
+      .duration(600)
+      .attr('fill', function(d, i) {
+        if ((i / frequencyData.length) < ($scope.audio.currentTime / $scope.duration)) {
+          return 'rgb(0, 0, ' + 220 + ')';
+        } else {
+          return 'rgb(0, 0, ' + 100 + ')';
+        }
+      });
+      renderComments();
+    }
 
   $scope.togglePlay = function () {
     if ($scope.audio.src) {
@@ -40,63 +139,8 @@ angular.module('jam.song', [])
         $scope.song.address;
       $scope.audio.play();
     }
-  };
+  };  
 
-  $scope.pinComment = function () {
-    $scope.commentTime = $scope.audio.currentTime / $scope.audio.duration;
-    $scope.pinningComment = true;
-  };
-
-  $scope.addComment = function (comment) {
-    var time = Math.floor($scope.commentTime * $scope.audio.duration);
-    Songs.addComment({note: comment, time: time, userId: $scope.user.id}, $scope.song.id);
-    $scope.pinningComment = false;
-    $scope.comment = '';
-  };
-
-  $scope.setPlayTime = function (e) {
-    div = document.getElementsByClassName('visualizer')[0];
-    var x = e.clientX - div.offsetLeft;
-    
-    $scope.audio.currentTime = $scope.audio.duration * x / svgWidth;
-  };
-  function createSvg(parent, height, width) {
-    return d3.select(parent).append('svg').attr('height', height).attr('width', width);
-  }
-
-  var svg = createSvg('.visualizer', svgHeight, svgWidth);
-
-  svg.selectAll('rect')
-    .data(frequencyData)
-    .enter()
-    .append('rect')
-    .attr('x', function (d, i) {
-     return i * (svgWidth / frequencyData.length);
-    })
-    .attr('width', svgWidth / frequencyData.length - barPadding);
-
-  // Continuously loop and update chart with frequency data.
-  function renderChart() {
-     // Update d3 chart with new data.
-     svg.selectAll('rect')
-        .data(frequencyData)
-        .attr('y', function(d) {
-           return svgHeight - d;
-        })
-        .attr('height', function(d) {
-           return d;
-        })
-        .transition()
-        .duration(600)
-        .attr('fill', function(d, i) {
-          if ((i / frequencyData.length) < ($scope.audio.currentTime / $scope.duration)) {
-            return 'rgb(0, 0, ' + 220 + ')';
-          } else {
-            return 'rgb(0, 0, ' + 100 + ')';
-          }
-        });
-  }
-
-  setInterval(renderChart, 300);
+  setInterval(renderFlow, 300);
 
 }]);
