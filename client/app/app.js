78d2f366fbc35825e937f10a4ac5fb5c402e5bdd
Erick Paepke <epaepke@gmail.com>
2016-03-29 14:53:11 -0700

    (feat) Update login errors notifications and add enter binding

--- client/app/app.js
+++ client/app/app.js
@@ -178,6 +178,18 @@ angular.module('jam', [
     }
   };
 })
+.directive('myEnter', function () {
+  return function (scope, element, attrs) {
+    element.bind("keydown keypress", function (event) {
+      if(event.which === 13) {
+        scope.$apply(function (){
+          scope.$eval(attrs.myEnter);
+        });
+        event.preventDefault();
+      }
+    });
+  };
+})
 .factory('AttachTokens', function ($window) {
   // http request interceptor adds tokens
   var attach = {
