cfc3ada46714b51d485b0ae16d2b093a91174c6d
Erick Paepke <epaepke@gmail.com>
2016-03-30 10:47:47 -0700

    (fix) Prevent users from inviting themselves to their own groups

--- client/app/groups/settings.js
+++ client/app/groups/settings.js
@@ -24,18 +24,19 @@ angular.module('jam.groupSettings', [])
 
   $scope.sendInvite = function() {
     $scope.sendingInvite = true;
-
-    Groups.sendInvite($scope.group, $scope.invite)
-    .then(function(res) {
-      $scope.invite = "";
-      $scope.inviteForm.$setPristine();
-      Groups.getGroupsData($scope.user, true)
-      .then(function() {
-        $scope.sendingInvite = false;
+    if ($scope.user.email !== $scope.invite) {
+      Groups.sendInvite($scope.group, $scope.invite)
+      .then(function(res) {
+        $scope.invite = "";
+        $scope.inviteForm.$setPristine();
+        Groups.getGroupsData($scope.user, true)
+        .then(function() {
+          $scope.sendingInvite = false;
+        })
+        .catch(console.error);
       })
       .catch(console.error);
-    })
-    .catch(console.error);
+    }
   };
 
   $scope.updateGroupProfile = function() {
