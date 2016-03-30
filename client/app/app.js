926853415dd0f23db0ee7340456c51fd17e8dec8
Nick Echols <stere.ereo@gmail.com>
2016-03-29 18:38:55 -0700

    (cleanup) fix linter warnings

--- client/app/app.js
+++ client/app/app.js
@@ -202,9 +202,9 @@ angular.module('jam', [
 })
 .directive('myEnter', function () {
   return function (scope, element, attrs) {
-    element.bind("keydown keypress", function (event) {
-      if(event.which === 13) {
-        scope.$apply(function (){
+    element.bind('keydown keypress', function (event) {
+      if (event.which === 13) {
+        scope.$apply(function () {
           scope.$eval(attrs.myEnter);
         });
         event.preventDefault();
