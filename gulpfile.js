0b2b390725002a96988599f3df152a424b7e4972
Nick Echols <stere.ereo@gmail.com>
2016-03-30 16:23:24 -0700

    (cleanup) pack all lib files into one js file, ui tweaks

--- gulpfile.js
+++ gulpfile.js
@@ -11,10 +11,23 @@ var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 var ngmin = require('gulp-ngmin');
 var prefix = require('gulp-autoprefixer');
+var size = require('gulp-size');
 
 // the paths to our app files
 var paths = {
   scripts: ['client/app/**/*.js'],
+  libsrc: [ 'client/lib/angular/angular.min.js',
+            'client/lib/angular-route/angular-route.min.js',
+            'client/lib/angular-animate/angular-animate.min.js',
+            'client/lib/ng-file-upload/ng-file-upload.min.js',
+            'client/lib/ng-img-crop-full-extended/compile/minified/ng-img-crop.js',
+            'client/lib/ngprogress/build/ngprogress.min.js',
+            'client/lib/underscore/underscore-min.js',
+            'client/lib/d3/d3.min.js',
+            'client/lib/angularjs-slider/dist/rzslider.min.js',
+            'client/lib/angular-drag-and-drop-lists/angular-drag-and-drop-lists.min.js',
+            'client/lib/ng-focus-if/focusIf.min.js'
+          ],
   html: ['client/app/**/*.html', 'client/index.html'],
   styles: ['client/app/styles/*.scss'],
   test: ['tests/**/*.js']
@@ -36,7 +49,15 @@ gulp.task('browser-sync', ['nodemon'], function() {
 });
 
 // Minify the things
-gulp.task('build', ['sass'], function() {
+gulp.task('buildlib', function() {
+  return gulp.src(paths.libsrc)
+    .pipe(size({showFiles: true}))
+    // .pipe(ngmin())
+    .pipe(concat('lib.min.js'))   // Combine into 1 file
+    .pipe(gulp.dest('client/dist'));            // Write non-minified to disk
+});
+
+gulp.task('build', ['sass', 'buildlib'], function() {
   return gulp.src(paths.scripts)
     .pipe(ngmin())
     .pipe(concat('main.js'))   // Combine into 1 file
