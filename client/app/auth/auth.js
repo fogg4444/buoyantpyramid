d0a2efc33619bed1b72eb52f792b84a6e4cfb98c
Erick Paepke <epaepke@gmail.com>
2016-05-07 14:10:45 -0700

    (fix) Auto populate email login field if email is in url

--- client/app/auth/auth.js
+++ client/app/auth/auth.js
@@ -7,10 +7,10 @@ function ($scope, $window, $location, $routeParams, Users) {
   $scope.loginError = '';
   $scope.signupError = '';
   $scope.user = {};
-  $scope.user.email = $routeParams.email || '';
 
   $scope.toggleLogin = function () {
-    $scope.user.email = $scope.user.password = '';
+    $scope.user.email = $routeParams.email || '';
+    $scope.user.password = '';
     $scope.loginForm.$setPristine();
     $scope.showLogin = !$scope.showLogin;
     if ($scope.showSignup && $scope.showLogin) {
@@ -79,5 +79,11 @@ function ($scope, $window, $location, $routeParams, Users) {
   $scope.backImage = {
     'background-image': 'url(assets/bands/' + $scope.images[Math.floor(Math.random() * $scope.images.length)] + ')'
   };
+  $scope.$on('$viewContentLoaded', function() {
+    if ($routeParams.email) {
+      $scope.toggleLogin();
+    }
+  });
+
 
 }]);
