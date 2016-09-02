94a29b56aae3c3b82f461415578140304fa4a42f
Brian Fogg <fogg4444@gmail.com>
2016-09-02 09:10:32 -0700

    (fix) comment out a few tests

--- test/client/unit/servicesTest.js
+++ test/client/unit/servicesTest.js
@@ -1,56 +1,58 @@
 // 'use strict';
 // var expect = require('chai').expect;
-// var dbModels = require('../../../client/services/services.js');
+// var SongFactory = require('../../../client/app/services/songsFactory');
 
 // describe('Services', function () {
-//   beforeEach(module('jam.services'));
+//   // beforeEach(module('jam.services'));
 
-//   afterEach(inject(function ($httpBackend) {
-//     $httpBackend.verifyNoOutstandingExpectation();
-//     $httpBackend.verifyNoOutstandingRequest();
-//   }));
+// //   afterEach(inject(function ($httpBackend) {
+// //     $httpBackend.verifyNoOutstandingExpectation();
+// //     $httpBackend.verifyNoOutstandingRequest();
+// //   }));
 
 //   describe('Songs Factory', function () {
-//     var $httpBackend, Songs;
+// //     var $httpBackend, Songs;
 
-//     beforeEach(inject(function (_$httpBackend_, _Songs_) {
-//       $httpBackend = _$httpBackend_;
-//       Songs = _Songs_;
-//     }));
+// //     beforeEach(inject(function (_$httpBackend_, _Songs_) {
+// //       $httpBackend = _$httpBackend_;
+// //       Songs = _Songs_;
+// //     }));
 
 //     it('should exist', function () {
-//       expect(Songs).to.exist;
+//       // console.log(SongFactory)
+//       expect(true).to.equal(true)
+//       // expect(SongFactory).to.exist;
 //     });
 
-//     it('should have a method `getAllSongs`', function () {
-//       expect(Songs.getAllSongs).to.be.a('function');
-//     });
+// //     it('should have a method `getAllSongs`', function () {
+// //       expect(Songs.getAllSongs).to.be.a('function');
+// //     });
 
-//     it('should have a method `addSong`', function () {
-//       expect(Songs.addSong).to.be.a('function');
-//     });
+// //     it('should have a method `addSong`', function () {
+// //       expect(Songs.addSong).to.be.a('function');
+// //     });
 
-//     it('should get all songs with `getAllSongs`', function () {
-//       var mockResponse = [
-//         { title: 'Some Song',
-//           url: 'https://song1.com' },
-//         { title: 'Another Song',
-//           url: 'https://song2.com' }
-//       ];
+// //     it('should get all songs with `getAllSongs`', function () {
+// //       var mockResponse = [
+// //         { title: 'Some Song',
+// //           url: 'https://song1.com' },
+// //         { title: 'Another Song',
+// //           url: 'https://song2.com' }
+// //       ];
 
-//       $httpBackend.expect('GET', '/api/groups/:id/songs/').respond(mockResponse);
+// //       $httpBackend.expect('GET', '/api/groups/:id/songs/').respond(mockResponse);
 
-//       Songs.getAllSongs().then(function (songs) {
-//         expect(songs).to.deep.equal(mockResponse);
-//       });
+// //       Songs.getAllSongs().then(function (songs) {
+// //         expect(songs).to.deep.equal(mockResponse);
+// //       });
 
-//       $httpBackend.flush();
-//     });
+// //       $httpBackend.flush();
+// //     });
 
-//     it('should add a new song with `addSong`', function () {
-//       // Send in ??? get back a url?
-//       expect(23).to.be.a('number');
-//     });
+// //     it('should add a new song with `addSong`', function () {
+// //       // Send in ??? get back a url?
+// //       expect(23).to.be.a('number');
+// //     });
 
 //   });
 
---
+++ test/client/unit/upload.spec.js
@@ -0,0 +1,30 @@
+// var expect = require('chai').expect;
+
+// describe('Upload Controller', function() {
+//   beforeEach(module('jam'));
+
+//   var $controller;
+
+//   beforeEach(inject(function(_$controller_){
+//     // The injector unwraps the underscores (_) from around the parameter names when matching
+//     $controller = _$controller_;
+//   }));
+
+  // describe('test test', function() {
+  //   it('should test true', function() {
+  //     expect(true).to.equal(true);
+  //   });
+  // });
+
+  
+
+//   describe('$scope.grade', function() {
+//     it('sets the strength to "strong" if the password length is >8 chars', function() {
+//       var $scope = {};
+//       var controller = $controller('PasswordController', { $scope: $scope });
+//       $scope.password = 'longerthaneightchars';
+//       $scope.grade();
+//       expect($scope.strength).toEqual('strong');
+//     });
+//   });
+// });
